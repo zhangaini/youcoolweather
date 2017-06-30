2ba8b8d5713b8f387cb0d19a1a156c3d9b0079df
zhangaini <903188706@qq.com>
2017-06-30 13:15:58 +0800

    second commit

---
+++ app/src/main/java/com/example/fafa/youcoolweather/db/City.java
@@ -0,0 +1,46 @@
+package com.example.fafa.youcoolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by zhang on 2017/6/30.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
